2ef895aa74fe408301a2aad97501a81e3f2fa7f7
trungpro2320 <danganhthanhtrung@gmail.com>
2017-04-21 13:42:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
 process.env.DEBUG = 'actions-on-google:*';
 const Assistant = require('actions-on-google').ApiAiAssistant;
-const ASK_TIME_INTENT = 'ask-for-the-time';  
-const CITY = 'geo-city';
-const ASK_TIME_INTENT = 'ask-for-the-time';  
 
-function whatTimeIsIt(assistant) {
-  var city = assistant.getArgument(CITY);
-  if (city === 'Paris') 
-    assistant.ask("It's noon in Paris.");
-  else if (city === 'London') 
-    assistant.ask("It's 11 a.m. in London.");
-  else 
-    assistant.ask("It’s way to early or way too late in " + city);
-}
-exports.agent = function(request, response) {
-    var assistant = new Assistant({request: request, response: response});
-    var actionMap = new Map();
-    actionMap.set(ASK_TIME_INTENT, whatTimeIsIt);
-    assistant.handleRequest(actionMap);
+// [START YourAction]
+exports.yourAction = (req, res) => {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  const actionMap = new Map();
+  actionMap.set('<API.AI_action_name>', responseHandler);
+
+  assistant.handleRequest(actionMap);
 };
+// [END YourAction]
